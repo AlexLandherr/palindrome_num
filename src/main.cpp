78f49d7c9936550e6b9daa02efc8bb471df66982
Alex Landherr <alex.landherr@gmail.com>
2023-09-12 15:23:46 +0000

    Init commit.

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/*
+A palindrome number looks the same whether you read it from the right or the left.
+The largest palindrome that is a product of two two-digit numbers is 9009 = 91 x 99.
+
+Find the largest palindrome number that is a product of two three-digit numbers.
+*/
+
+int main() {
+
+    return 0;
+}
